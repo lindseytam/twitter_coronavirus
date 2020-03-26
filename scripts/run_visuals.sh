cec0f38e0a228fee042d60a15d43650b18026232
lindseytam <42879918+lindseytam@users.noreply.github.com>
2020-03-25 23:47:08 -0700

    Update run_visuals.sh

--- scripts/run_visuals.sh
+++ scripts/run_visuals.sh
@@ -2,7 +2,6 @@
 
 # this command creates a log folder if it doesn't already exist
 mkdir -p visuals
-date
 # by default, python uses what's called "buffered" output;
 # buffered output causes python to 
 export PYTHONUNBUFFERED=True
@@ -11,7 +10,10 @@ export PYTHONUNBUFFERED=True
 # this loop processes each file in /data/twitter_corona
 # and passes the file to your map.py file as input
 # for path in /data/tweets_corona/*; do
-for hashtag in '#코로나바이러스' '#コロナウイルス' '#冠状病毒' '#covid2019' '#covid-2019' '#covid19' do
+declare -a StringArray=("#코로나바이러스" "#コロナウイルス" "#冠状病毒" "#covid2019" "#covid-2019" \
+                        "#covid19" "#covid-19" "#coronavirus" "#corona" "#virus" "#flu"\
+                        "#sick" "#cough" "#sneeze" "#hospital" "#nurse" "#doctor")
+for val in ${StringArray[@]}; do
 
     # extracts the filename from the path variable
 #     filename=$(basename "$path")

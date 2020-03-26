f375f0fccbed111e4b4ed2b15bae4bf98aa27dc7
lindseytam <42879918+lindseytam@users.noreply.github.com>
2020-03-25 23:32:37 -0700

    Update run_visuals.sh

--- scripts/run_visuals.sh
+++ scripts/run_visuals.sh
@@ -2,27 +2,6 @@
 
 # this command creates a log folder if it doesn't already exist
 mkdir -p visuals
-
-# all hashtags
-# hashtags = [
-#     '#코로나바이러스',  # korean
-#     '#コロナウイルス',  # japanese
-#     '#冠状病毒',        # chinese
-#     '#covid2019',
-#     '#covid-2019',
-#     '#covid19',
-#     '#covid-19',
-#     '#coronavirus',
-#     '#corona',
-#     '#virus',
-#     '#flu',
-#     '#sick',
-#     '#cough',
-#     '#sneeze',
-#     '#hospital',
-#     '#nurse',
-#     '#doctor',
-#     ]
 date
 # by default, python uses what's called "buffered" output;
 # buffered output causes python to 
@@ -32,24 +11,7 @@ export PYTHONUNBUFFERED=True
 # this loop processes each file in /data/twitter_corona
 # and passes the file to your map.py file as input
 # for path in /data/tweets_corona/*; do
-for hashtag in '#코로나바이러스' \ # korean
-    '#コロナウイルス' \ # japanese
-    '#冠状病毒' \        # chinese
-    '#covid2019' \
-    '#covid-2019' \
-    '#covid19' \
-    '#covid-19' \
-    '#coronavirus' \
-    '#corona' \
-    '#virus' \
-    '#flu' \
-    '#sick' \
-    '#cough' \
-    '#sneeze' \
-    '#hospital' \
-    '#nurse' \
-    '#doctor' 
-    do
+for hashtag in '#코로나바이러스' '#コロナウイルス' '#冠状病毒' '#covid2019' '#covid-2019' '#covid19' do
 
     # extracts the filename from the path variable
 #     filename=$(basename "$path")
